f2c153e09b614664377e8c6055e3778f87840475
ZH <chongzhanhang2000@gmail.com>
2022-12-28 14:42:37 +0800

    Changed icons

--- icare/src/components/Navbar.js
+++ icare/src/components/Navbar.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import Appointments from '../images/Appointments.png'
-import Home from '../images/Home.png'
-import Logout from '../images/Logout.png'
-import Notifications from '../images/Notifications.png'
-import Patient from '../images/Patient.png'
-import Treatment from '../images/Treatment.png';
+
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MedicationIcon from '@mui/icons-material/Medication';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Navbar(){
     return(
         <div className='fixed top-0 left-0 h-screen w-16 
                         m-0 flex flex-col text-white bg-primary
                         shadow-lg'>
-            <NavbarIcon icon={Home} text="Home"/>
-            <NavbarIcon icon={Appointments} text="Appointments"/>
-            <NavbarIcon icon={Treatment} text="Treatment"/>
-            <NavbarIcon icon={Notifications} text="Notifications"/>
-            <NavbarIcon icon={Logout} text="Logout"/>
+            <NavbarIcon icon={<HomeIcon />} text="Home"/>
+            <NavbarIcon icon={<CalendarMonthIcon />} text="Appointments"/>
+            <NavbarIcon icon={<MedicationIcon />} text="Treatment"/>
+            <NavbarIcon icon={<NotificationsIcon />} text="Notifications"/>
+            <NavbarIcon icon={<LogoutIcon />} text="Logout"/>
         </div>
     );
 };
 
 const NavbarIcon = ({icon, text}) => (
     <div className='navbar--icon group'>
-        <img src={icon} />
+        {icon}
         <span className='navbar--text group-hover:scale-100'>
             {text}
         </span>
