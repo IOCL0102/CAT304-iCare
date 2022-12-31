6ca51bed05dfaa727f787babe59b5bc967d5cfb9
IvanOoi <ivanooichenliang@gmail.com>
2022-12-31 11:42:54 +0800

    Added z-index so that nav text always show in front

--- icare/src/components/doctorNavbar.js
+++ icare/src/components/doctorNavbar.js
@@ -6,9 +6,9 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PermContactCalendarIcon from '@mui/icons-material/PermContactCalendar';
 
-export default function Navbar(){
+export default function DocNavbar(){
     return(
-        <div className='fixed top-0 left-0 h-screen w-16 
+        <div className='relative top-0 left-0 h-screen w-16 
                         m-0 flex flex-col text-white bg-primary
                         shadow-lg'>
             <NavbarIcon icon={<DashboardIcon />} text="Dashboard"/>
@@ -23,7 +23,7 @@ export default function Navbar(){
 const NavbarIcon = ({icon, text}) => (
     <div className='navbar--icon group'>
         {icon}
-        <span className='navbar--text group-hover:scale-100'>
+        <span className='navbar--text group-hover:scale-100 z-50'>
             {text}
         </span>
     </div>
