99220f48e6333433f7cf0c671dc09db7b4065f5c
IvanOoi <ivanooichenliang@gmail.com>
2023-01-08 12:46:47 +0800

    Created simple average patient visit graph

---
+++ icare/src/components/DrDashboardGraph/AvgPatientVisitGraph.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  maintainAspectRatio : false,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+    },
+  },
+};
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Patient visit',
+      data: [102,2500,3580,2396,5000,1020,7658],
+      backgroundColor: '#C5D8FF',
+    }
+  ],
+};
+
+export default function AvgPatientVisitGraph(){
+    
+    return(
+        <>
+            <Bar data={data} options={options}  />
+        </>
+
+    );
+}
