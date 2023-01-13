d036b5bb5efb6aae9a3ebcc8493477c1c30db297
IvanOoi <ivanooichenliang@gmail.com>
2023-01-13 23:36:51 +0800

    New - Added Confirmation Pop Up functionality for Appointment Request

--- icare/src/components/DrAppointmentComp/AppointmentReq.js
+++ icare/src/components/DrAppointmentComp/AppointmentReq.js
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 
-export default function AppointmentReq(){
-    
+/*
+    useEffect(() => {
+        {console.log("clicked")}
+      return () => {
+        isAppointmentBtnClicked ? (
+            <div className='fixed z-10 bg-green-500/40 cursor-pointer inset-0' >
+                <div className='z-20 ml-16'>
+                    <h1>hihi</h1>
+                </div>
+                <button onClick={setAppointmentBtn(false)}></button>
+            </div>
+        )
+        : <h1 className="bg-green-500">FALSEEEEE</h1>
+      }
+    }, [isAppointmentBtnClicked])
+*/
+
+export default function AppointmentReq({setPopUpFunc, setPopUpContent}){
+
     const [appointmentRequests, setAppointmentReq] = useState([
         {
             id: 1,
             name: "Simon Alix",
+            gender: "Male",
+            age: "35",
             date: "1 Sept 2022",
             time: "04:00 PM",
             link: "#",
             appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
             timePassed: "5 minutes",
             profilePic: "https://placeimg.com/160/160/arch",
+            lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+            observation: "High fever and cough at normal hemoglobin levels. ",
+            prescription: `Paracetmol - 2 times a day
+                            Dizopam - Day and Night before mealw
+                            Wikoryl`,
         },
         {
             id: 4,
@@ -33,7 +57,7 @@ export default function AppointmentReq(){
             link: "#",
             appointPurpose: "I am not feeling well so i plan to do the medical check up",
             timePassed: "2 hours ago",
-            profilePic: "https://placeimg.com/160/160/arch",
+            profilePic: "https://placeimg.com/160/160/any",
         },
         {
             id: 2,
@@ -43,7 +67,7 @@ export default function AppointmentReq(){
             link: "#",
             appointPurpose: "I am not feeling well so i plan to do the medical check up",
             timePassed: "1 day ago",
-            profilePic: "https://placeimg.com/160/160/arch",
+            profilePic: "https://placeimg.com/160/160/any",
         },
         {
             id: 3,
@@ -56,7 +80,7 @@ export default function AppointmentReq(){
             profilePic: "https://placeimg.com/160/160/arch",
         },
     ]);
-
+    
     return(
         <div className="flex flex-col ">
             <h1 className="font-semibold text-2xl text-center my-5">Appointment Request</h1>
@@ -67,35 +91,40 @@ export default function AppointmentReq(){
                 <div className='grow text-center my-5'>
                     <u className='text-base underline-offset-1 hover:cursor-pointer hover:text-blue-500'>view all</u>
                 </div>
-            </div>
-
-            
-        </div>
+            </div>            
+        </div> 
     );
-};
 
-const AppointmentRequestBar = ({data}) => {
-    return(
-        <div className='flex flex-row bg-sky-100 rounded-xl px-3 py-2'>
-            <div className="mask mask-circle h-11 w-11 ml-2 mr-5 self-center shrink-0">
-                <img src={data.profilePic} className='object-fill' />
-            </div>
 
-            <div className='grow mr-5'>
-                <h1 className='text-lg font-base '> {data.name}</h1>
-                <h3 className="font-base text-sm text-blue-600 bg-blue-200 rounded-2xl px-3 text-justify tracking-tight">{data.date} | {data.time} </h3>
-                <h3 className="text-sm ">{data.appointPurpose.length > 20 ? data.appointPurpose.slice(0,50) + " ..." : data.appointPurpose}</h3>
-            </div>
-
-            <div className="flex flex-col">
-                <div className="flex flex-row gap-3 grow justify-end ">
-                    <CheckIcon fontSize="large" className="text-white bg-green-500 rounded-full cursor-pointer self-center"/>
-                    <ClearIcon fontSize="large" className="text-white bg-red-500 rounded-full cursor-pointer self-center"/>
+    function AppointmentRequestBar({data}){
+        return(
+            <div className='flex flex-row bg-sky-100 rounded-xl px-3 py-2'>
+                <div className="mask mask-circle h-11 w-11 ml-2 mr-5 self-center shrink-0">
+                    <img src={data.profilePic} className='object-fill' />
                 </div>
-           
-                <h3 className="text-xs text-end mt-2">{data.timePassed}</h3>
+
+                <div className='grow mr-5'>
+                    <h1 className='text-lg font-base '> {data.name}</h1>
+                    <h3 className="font-base text-sm text-blue-600 bg-blue-200 rounded-2xl px-3 text-justify tracking-tight">{data.date} | {data.time} </h3>
+                    <h3 className="text-sm ">{data.appointPurpose.length > 20 ? data.appointPurpose.slice(0,25) + " ..." : data.appointPurpose}</h3>
+                </div>
+
+                <div className="flex flex-col">
+                    <div className="flex flex-row gap-3 grow justify-end ">
+                        <CheckIcon fontSize="large" onClick={()=>{setPopUpFunc(true); data.btnClicked="accept"; setPopUpContent(data)}} className="text-white bg-green-500 rounded-full cursor-pointer self-center hover:scale-110"/>
+                        <ClearIcon fontSize="large" onClick={()=>{setPopUpFunc(true); data.btnClicked="reject"; setPopUpContent(data)}} className="text-white bg-red-500 rounded-full cursor-pointer self-center hover:scale-110"/>
+                    </div>
+            
+                    <h3 className="text-xs text-end mt-2 opacity-70">{data.timePassed}</h3>
+                </div>
             </div>
-        </div>
-    );
+        );
+    };
 };
+
+
+
+
+
+
 
--- icare/src/pages/DoctorAppointPage.js
+++ icare/src/pages/DoctorAppointPage.js
@@ -1,12 +1,18 @@
 import PatientList from '../components/DrAppointmentComp/PatientList';
+import { PatientDetailSummary } from '../components/DrAppointmentComp/PatientList';
 import AppointmentReq from '../components/DrAppointmentComp/AppointmentReq';
 import { useState } from 'react';
-// bg-slate-100
-// style={{gridTemplateColumns: 'repeat(autofill, minmax(250px, 1fr))' }}
 
 export default function DoctorAppointPage(){
+
+    const [isShowPopOut, setIsShowPopOut] = useState(false);
+    const [popOutContent, setPopOutContent] = useState({});
+
     return(
+        
         <div className='grid ml-16 grid-cols-3'  >
+
+            { isShowPopOut ? <PopUpContent /> : <></> }
 
 
             <div className='col-span-2'>
@@ -17,18 +23,57 @@ export default function DoctorAppointPage(){
 
             <div className='col-span-1'>
                 <div className='rounded-3xl bg-white m-5 ml-2 px-5 py-2 drop-shadow-xl'>
-                    <AppointmentReq />
+                    <AppointmentReq setPopUpFunc={setIsShowPopOut} setPopUpContent={setPopOutContent} />
                 </div>
             </div>
 
             <div className='col-span-2 bg-green-500'>
-            <AppointmentReq />
+                <AppointmentReq />
             </div>
 
             <div className='col-span-1 bg-red-500'>
-            <AppointmentReq />
+                <AppointmentReq />
             </div>
 
         </div>
     );
+
+    function PopUpContent(){
+        return(
+            <div className='fixed z-10 bg-black/10 inset-0 ml-16 flex justify-center items-center '>
+                <div className='flex flex-col z-20 bg-sky-100/90 h-3/4 w-3/4 rounded-3xl'>
+   
+                    <div className='flex flex-row p-5 ' >
+                        <div className='flex flex-col justify-center justify-items-center'>
+                            <div className="mask mask-circle h-2/4 self-center shrink-0 mb-5 mx-5">
+                                <img src={popOutContent.profilePic} className='object-fill' />
+                            </div>
+                            <h1 className='text-center text-xl font-semibold'>{popOutContent.name}</h1>
+                            <h2 className='text-center text-base font-light text-gray-500'>{popOutContent.gender} - {popOutContent.age} Years old</h2>
+                        </div>
+
+                        <div className='bg-white m-5 p-5 border-2 border-sky-400 rounded-3xl'>
+                            <PatientDetailSummary consultationData = {popOutContent} />
+                        </div>
+                    </div>
+
+                    <div className='flex flex-row justify-center bg-blue-300/80 py-3'>
+                        <h1 className='inline mr-10 text-xl font-bold'>Date : {popOutContent.date}</h1>
+                        <h1 className='inline text-xl font-bold'>Time : {popOutContent.time}</h1>
+                    </div>
+
+                    <h1 className='text-center text-semibold text-xl mt-5 inline'> Are you sure you want to  
+                    <h1 className={popOutContent.btnClicked=="accept"? "text-green-600 inline font-bold" : "text-red-500 inline font-bold"}> {popOutContent.btnClicked.toUpperCase()}</h1> the appointment request?</h1>
+
+                    <div className='flex flex-row justify-center'>
+                        <button className="btn bg-green-500 cursor-pointer m-5 mr-20
+                        hover:scale-105 hover:bg-green-600 " onClick={()=>{setIsShowPopOut(true)}}>Yes</button>
+                        <button className="btn bg-red-500 cursor-pointer m-5 mr-20
+                        hover:scale-105 hover:bg-red-600" onClick={()=>{setIsShowPopOut(false)}}>No</button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 }
+
