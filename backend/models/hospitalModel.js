bfb5047d10362122e091a461e15e26b80e6a16f5
JacksonLiew101 <58961556+JacksonLiew101@users.noreply.github.com>
2023-01-15 03:08:56 +0800

    feat: add hospital Mongoose model

---
+++ backend/models/hospitalModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const hospitalSchema = new Schema({
+    facility_name: {
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number, // later change to support decimal/double
+        required: true
+    },
+    longitude: {
+        type: Number, // later change to support decimal/double
+        required: true
+    },
+    schema_ver: {
+        type: Number,
+        required: true,
+        default: 1.0
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Hospital', hospitalSchema);
