2c37da06567f8674980e889e2cded7e64ffd8647
IvanOoi <ivanooichenliang@gmail.com>
2023-01-16 13:52:52 +0800

    Modify - Assign a variable for the AppointmentReq data

--- icare/src/components/DrAppointmentComp/AppointmentReq.js
+++ icare/src/components/DrAppointmentComp/AppointmentReq.js
@@ -18,73 +18,74 @@ import ClearIcon from '@mui/icons-material/Clear';
       }
     }, [isAppointmentBtnClicked])
 */
+const appointmentRequestTable = [ {
+    id: 1,
+    name: "Simon Alix",
+    gender: "Male",
+    age: "35",
+    date: "1 Sept 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+    timePassed: "5 minutes",
+    profilePic: "https://placeimg.com/160/160/arch",
+    lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+    observation: "High fever and cough at normal hemoglobin levels. ",
+    prescription: `Paracetmol - 2 times a day
+                    Dizopam - Day and Night before mealw
+                    Wikoryl`,
+},
+{
+    id: 4,
+    name: "Chirstine",
+    date: "3 Apr 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "58 minutes ago",
+    profilePic: "https://placeimg.com/160/160/arch",
+},
+{
+    id: 5,
+    name: "Chirstine",
+    date: "3 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "2 hours ago",
+    profilePic: "https://placeimg.com/160/160/any",
+},
+{
+    id: 2,
+    name: "Abu Bakar",
+    date: "2 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "1 day ago",
+    profilePic: "https://placeimg.com/160/160/any",
+},
+{
+    id: 3,
+    name: "Chirstine",
+    date: "3 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "2 days ago",
+    profilePic: "https://placeimg.com/160/160/arch",
+},
+];
+appointmentRequestTable.push(...appointmentRequestTable);
 
 export default function AppointmentReq({setPopUpFunc, setPopUpContent}){
 
-    const [appointmentRequests, setAppointmentReq] = useState([
-        {
-            id: 1,
-            name: "Simon Alix",
-            gender: "Male",
-            age: "35",
-            date: "1 Sept 2022",
-            time: "04:00 PM",
-            link: "#",
-            appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
-            timePassed: "5 minutes",
-            profilePic: "https://placeimg.com/160/160/arch",
-            lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
-            observation: "High fever and cough at normal hemoglobin levels. ",
-            prescription: `Paracetmol - 2 times a day
-                            Dizopam - Day and Night before mealw
-                            Wikoryl`,
-        },
-        {
-            id: 4,
-            name: "Chirstine",
-            date: "3 Apr 2022",
-            time: "04:00 PM",
-            link: "#",
-            appointPurpose: "I am not feeling well so i plan to do the medical check up",
-            timePassed: "58 minutes ago",
-            profilePic: "https://placeimg.com/160/160/arch",
-        },
-        {
-            id: 5,
-            name: "Chirstine",
-            date: "3 April 2022",
-            time: "04:00 PM",
-            link: "#",
-            appointPurpose: "I am not feeling well so i plan to do the medical check up",
-            timePassed: "2 hours ago",
-            profilePic: "https://placeimg.com/160/160/any",
-        },
-        {
-            id: 2,
-            name: "Abu Bakar",
-            date: "2 April 2022",
-            time: "04:00 PM",
-            link: "#",
-            appointPurpose: "I am not feeling well so i plan to do the medical check up",
-            timePassed: "1 day ago",
-            profilePic: "https://placeimg.com/160/160/any",
-        },
-        {
-            id: 3,
-            name: "Chirstine",
-            date: "3 April 2022",
-            time: "04:00 PM",
-            link: "#",
-            appointPurpose: "I am not feeling well so i plan to do the medical check up",
-            timePassed: "2 days ago",
-            profilePic: "https://placeimg.com/160/160/arch",
-        },
-    ]);
+    const [appointmentRequests, setAppointmentReq] = useState(appointmentRequestTable);
     
     return(
-        <div className="flex flex-col ">
-            <h1 className="font-semibold text-2xl text-center my-5">Appointment Request</h1>
-            <div className="flex flex-col gap-3">
+        <div className="flex flex-col pb-3">
+            <h1 className="font-semibold text-2xl text-center my-5 ">Appointment Request</h1>
+            <div className="flex flex-col gap-3 overflow-y-scroll" style={{height: '35rem'}}>
                 {appointmentRequests.map((appointmentRequest)=>(
                         <AppointmentRequestBar key={appointmentRequest.id} data={appointmentRequest}/>
                 ))}
