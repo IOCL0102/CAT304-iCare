76b24b0fcc56d907a657195b5901d05f23e3a39c
IvanOoi <ivanooichenliang@gmail.com>
2023-01-20 13:34:34 +0800

    Modify - Pass props into patient list and AppointmentReq component

--- icare/src/components/DrAppointmentComp/AppointmentReq.js
+++ icare/src/components/DrAppointmentComp/AppointmentReq.js
@@ -1,84 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
 
-/*
-    useEffect(() => {
-        {console.log("clicked")}
-      return () => {
-        isAppointmentBtnClicked ? (
-            <div className='fixed z-10 bg-green-500/40 cursor-pointer inset-0' >
-                <div className='z-20 ml-16'>
-                    <h1>hihi</h1>
-                </div>
-                <button onClick={setAppointmentBtn(false)}></button>
-            </div>
-        )
-        : <h1 className="bg-green-500">FALSEEEEE</h1>
-      }
-    }, [isAppointmentBtnClicked])
-*/
-const appointmentRequestTable = [ {
-    id: 1,
-    name: "Simon Alix",
-    gender: "Male",
-    age: "35",
-    date: "1 Sept 2022",
-    time: "04:00 PM",
-    link: "#",
-    appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
-    timePassed: "5 minutes",
-    profilePic: "https://placeimg.com/160/160/arch",
-    lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
-    observation: "High fever and cough at normal hemoglobin levels. ",
-    prescription: `Paracetmol - 2 times a day
-                    Dizopam - Day and Night before mealw
-                    Wikoryl`,
-},
-{
-    id: 4,
-    name: "Chirstine",
-    date: "3 Apr 2022",
-    time: "04:00 PM",
-    link: "#",
-    appointPurpose: "I am not feeling well so i plan to do the medical check up",
-    timePassed: "58 minutes ago",
-    profilePic: "https://placeimg.com/160/160/arch",
-},
-{
-    id: 5,
-    name: "Chirstine",
-    date: "3 April 2022",
-    time: "04:00 PM",
-    link: "#",
-    appointPurpose: "I am not feeling well so i plan to do the medical check up",
-    timePassed: "2 hours ago",
-    profilePic: "https://placeimg.com/160/160/any",
-},
-{
-    id: 2,
-    name: "Abu Bakar",
-    date: "2 April 2022",
-    time: "04:00 PM",
-    link: "#",
-    appointPurpose: "I am not feeling well so i plan to do the medical check up",
-    timePassed: "1 day ago",
-    profilePic: "https://placeimg.com/160/160/any",
-},
-{
-    id: 3,
-    name: "Chirstine",
-    date: "3 April 2022",
-    time: "04:00 PM",
-    link: "#",
-    appointPurpose: "I am not feeling well so i plan to do the medical check up",
-    timePassed: "2 days ago",
-    profilePic: "https://placeimg.com/160/160/arch",
-},
-];
-appointmentRequestTable.push(...appointmentRequestTable);
-
-export default function AppointmentReq({setPopUpFunc, setPopUpContent}){
+export default function AppointmentReq({appointmentRequestTable, setPopUpFunc, setPopUpContent}){
 
     const [appointmentRequests, setAppointmentReq] = useState(appointmentRequestTable);
     
@@ -104,7 +28,7 @@ export default function AppointmentReq({setPopUpFunc, setPopUpContent}){
                 <div className='grow mr-5'>
                     <h1 className='text-lg font-base '> {data.name}</h1>
                     <h3 className="font-base text-sm text-blue-600 bg-blue-200 rounded-2xl px-3 text-justify tracking-tight">{data.date} | {data.time} </h3>
-                    <h3 className="text-sm ">{data.appointPurpose.length > 20 ? data.appointPurpose.slice(0,25) + " ..." : data.appointPurpose}</h3>
+                    <h3 className="text-sm ">{data.appointPurpose.length > 50 ? data.appointPurpose.slice(0,47) + " ..." : data.appointPurpose}</h3>
                 </div>
 
                 <div className="flex flex-col">
--- icare/src/pages/DoctorAppointPage.js
+++ icare/src/pages/DoctorAppointPage.js
@@ -4,6 +4,192 @@ import AppointmentReq from '../components/DrAppointmentComp/AppointmentReq';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const appointmentRequestTable = [ {
+    id: 1,
+    name: "Simon Alix",
+    gender: "Male",
+    age: "35",
+    date: "1 Sept 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+    timePassed: "5 minutes",
+    profilePic: "https://placeimg.com/160/160/arch",
+    lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+    observation: "High fever and cough at normal hemoglobin levels. ",
+    prescription: `Paracetmol - 2 times a day
+                    Dizopam - Day and Night before mealw
+                    Wikoryl`,
+},
+{
+    id: 4,
+    name: "Chirstine",
+    date: "3 Apr 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "58 minutes ago",
+    profilePic: "https://placeimg.com/160/160/arch",
+},
+{
+    id: 5,
+    name: "Chirstine",
+    date: "3 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "2 hours ago",
+    profilePic: "https://placeimg.com/160/160/any",
+},
+{
+    id: 2,
+    name: "Abu Bakar",
+    date: "2 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "1 day ago",
+    profilePic: "https://placeimg.com/160/160/any",
+},
+{
+    id: 3,
+    name: "Chirstine",
+    date: "3 April 2022",
+    time: "04:00 PM",
+    link: "#",
+    appointPurpose: "I am not feeling well so i plan to do the medical check up",
+    timePassed: "2 days ago",
+    profilePic: "https://placeimg.com/160/160/arch",
+},
+];
+appointmentRequestTable.push(...appointmentRequestTable);
+
+const patientLists = [
+    {
+        id: 1,
+        name: "Simon Alix",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: `É vital, porém não derrubará aviões.
+        It's vital, but it won't shoot down aircraft.
+        Boa idéia, porém não funcionará.
+        Good idea, but it won't work.
+        Então não arriscarei nenhum... porém infelizmente estou tentada.
+        Then I shall venture none... however sorely I am tempted.`,
+        profilePic: "https://placeimg.com/160/160/any",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 2,
+        name: "Alix",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/any",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 3,
+        name: "Abu Bakar",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/any",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day Dizopam - Day and Night before mealw aaa Wikoryl`,
+    },
+    {
+        id: 4,
+        name: "Ahamd bin Yusoff Ali",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/arch",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 5,
+        name: "Ng Wei Kang",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/arch",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 6,
+        name: "Ng Wei Kang",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/arch",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 7,
+        name: "Ng Wei Kang",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/arch",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+    {
+        id: 8,
+        name: "Ng Wei Kang",
+        time: "04:00 PM",
+        link: "#",
+        appointPurpose: "Daily check up because I feel like my chest feel pain sometimes",
+        profilePic: "https://placeimg.com/160/160/arch",
+        gender: "Male",
+        age: 56,
+        lastChecked: "Dr Everly on 21 April 2021 Prescription #2J983KT0",
+        observation: "High fever and cough at normal hemoglobin levels. ",
+        prescription: `Paracetmol - 2 times a day
+                        Dizopam - Day and Night before mealw
+                        Wikoryl`,
+    },
+];
+
 export default function DoctorAppointPage(){
     const navigate = useNavigate();
     const [isShowPopOut, setIsShowPopOut] = useState(false);
@@ -25,13 +211,13 @@ export default function DoctorAppointPage(){
 
             <div className='col-span-2'>
                 <div className='bg-white rounded-3xl m-5 px-5 pt-2 pb-5 drop-shadow-xl '>
-                    <PatientList />
+                    <PatientList patientListTable={patientLists}/>
                 </div>
             </div>
 
             <div className='col-span-1'>
                 <div className='rounded-3xl bg-white m-5 ml-2 px-5 pt-2 pb-3 drop-shadow-xl '>
-                    <AppointmentReq setPopUpFunc={setIsShowPopOut} setPopUpContent={setPopOutContent} />
+                    <AppointmentReq appointmentRequestTable={appointmentRequestTable} setPopUpFunc={setIsShowPopOut} setPopUpContent={setPopOutContent} />
                 </div>
             </div>
 
