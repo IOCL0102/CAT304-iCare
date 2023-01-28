e9910d3eb2a5d34897fa3f7e995c997ec7067b28
thamsiewteng <siewtengtham@gmail.com>
2023-01-28 20:48:34 +0800

    Renamed search bar to notiuserbar

--- icare/src/pages/ViewPatientListPage.js
+++ icare/src/pages/ViewPatientListPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import DocNavbar from '../components/doctorNavbar';
-import SearchBar from '../components/SearchBar';
-import icare from "../images/iCare.png"
+import NotiUserBar from '../components/NotificationProfile';
 import { useState,useEffect } from 'react';
 import ReactPaginate from "react-paginate";
-
+import {useNavigate} from 'react-router-dom';
 
 const iconStyle = {
     "&.MuiSvgIcon-root": {
@@ -114,7 +113,7 @@ export default function ViewPatientListPage (){
   
     const displayUsers = patientCardInfo.slice(pagesVisited, pagesVisited + usersPerPage).map((patientCardInfo) => {
         return (
-            <div className='mr-10 ml-20 flex-col bg-blue-500'>           
+            <div className='mr-10 ml-20 flex-col'>           
                 <button type="button" class="w-56 h-48 p-8 ml-50 justify-items-center text-sm font-medium text-black bg-secondary rounded-lg border border-secondary hover:bg-secondary">
                 <img src={patientCardInfo.patientProfilePic} alt="" class="realtive ml-8 w-24 h-24 rounded-lg"/>{patientCardInfo.patientName}</button>
             </div>
@@ -128,11 +127,11 @@ export default function ViewPatientListPage (){
     };
 
     
-
+    let navigate = useNavigate();
     return (
         <div>
             <DocNavbar/>
-            <SearchBar/>  
+            <NotiUserBar/>  
             {/*============================ SEARCH FUNCTION ============================ */}
             <form class="flex items-center ml-20">   
                 <label for="simple-search" class="sr-only">Search</label>
@@ -142,20 +141,20 @@ export default function ViewPatientListPage (){
                 </div>
             </form>
             {/*============================ HEADING & ADD NEW PATIENT BUTTON ============================ */}
-            <div className ='bg-yellow-500 ml-20 mr-10 place-content-between flex p-2'>
+            <div className =' ml-20 mr-10 place-content-between flex p-2'>
                 <h1 className="items-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Patient List</h1>
                 <div className='flex items-end'>
-                    <button type="button" class="w-40 h-10 ml-30 text-sm font-medium text-white bg-primary rounded-lg border border-primary hover:bg-primary">Add new patient</button>
+                    <button type="button" onClick={()=>{navigate("/addnewpatientList")} } class="w-40 h-10 ml-30 text-sm font-medium text-white bg-primary rounded-lg border border-primary hover:bg-primary">Add new patient</button>
                 </div>  
             </div>
 
             {/*============================ DISPLAY PATIENT CARD ============================ */}
-            <div className="ml-0 mr-20 grid grid-cols-5 gap-4 bg-red-500">
+            <div className="ml-0 mr-20 grid grid-cols-5 gap-4">
                 {displayUsers}
             </div>
             
             {/*============================ PAGINATION ============================ */}
-            <div className='bg-purple-500 mt-5 ml-20 mr-40 flex justify-center'> 
+            <div className=' mt-5 ml-20 mr-40 flex justify-center'> 
                 <ReactPaginate
                 previousLabel={"<" }
                 nextLabel={">"} 
