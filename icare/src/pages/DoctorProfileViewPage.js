f82f4efd38b7595f33a36c35592b4f099cb3944a
JacksonLiew101 <58961556+JacksonLiew101@users.noreply.github.com>
2023-01-30 00:39:52 +0800

    fix: finally complete fetch data from backend

--- icare/src/pages/DoctorProfileViewPage.js
+++ icare/src/pages/DoctorProfileViewPage.js
@@ -14,22 +14,36 @@ const iconStyle = {
 export default function DoctorProfileViewPage () {
     let navigate = useNavigate();
     const [doctor, setDoctor] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null)
     const { user } = useAuthContext();
-    useEffect(() => {
-        const fetchDoctor = async () => {
-            const response = await fetch('/api/profile/', {
-                headers: {'Authorization': `Bearer ${user.token}`},
-            });
-            const json = await response.json();
-            console.log('able to fetch json: ', json)
-            if (response.ok) {
-                setDoctor(json);
-                console.log('able to set Doctor: ', doctor)
-            }
-        };
-        if (user) {
-            fetchDoctor();
+
+    const fetchDoctor = async () => {
+        // starting the fetch request
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/profile/', {
+            headers: {'Authorization': `Bearer ${user.token}`},
+        });
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }else{
+            // setDoctor({...doctor, json});
+            setDoctor(json)
+
+            setIsLoading(false)
         }
+        
+    };
+
+    useEffect(() => {
+        if(user){
+            fetchDoctor();
+        } 
     }, []);
 
     const DoctorInfo ={
@@ -44,10 +58,34 @@ export default function DoctorProfileViewPage () {
         doctorWorkingHoursEnd:"08:00 PM",
     };
 
+    if (isLoading) {
+        return (
+            <div>
+                {/* Add loading commponent here */}
+                <p> Loading .. </p>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        )
+    }
+
+    if (!doctor){
+        return (
+            <div>
+                <p> Doctor details unable to fetched </p>
+            </div>
+        )
+    }
+
     return (
     <div>
         <DocNavbar></DocNavbar>
-
         {/*============================ BACK BUTTON TO DASHBOARD============================ */}
         <div className=' p-3 flex justify-start ml-20 space-x-5'>
             <button type="button" onClick={()=>{navigate("/viewpatientlist")} }className="w-11 h-11 flex-wrap justify-center text-sm text-white bg-primary rounded-lg  ">
@@ -55,17 +93,17 @@ export default function DoctorProfileViewPage () {
             </button> 
         </div>
         <div className="mt-5" >
-            <img src={DoctorInfo.doctorProfilePic} alt="" className="absolute ml-48 w-30 h-30 rounded-lg"/>
+            <img src={doctor.photo} alt="" className="absolute ml-48 w-30 h-30 rounded-lg object-cover max-w-sm max-h-sm"/>
             <div className="flex flex-row justify-center space-x-1 form-control ">
                 <div className=" ml-20 mr-10 flex space-y-2 w-1/3 form-control ">
-                    <p className='text-xl text-black font-semibold block'>Name: {DoctorInfo.doctorName}</p>
-                    <p className='text-xl text-black font-semibold block'>Email: {DoctorInfo.doctorEmail}</p>
-                    <p className='text-xl text-black font-semibold block'>Phone: {DoctorInfo.doctorPhone}</p>
-                    <p className='text-xl text-black font-semibold block'>Working Experience: {DoctorInfo.doctorWorkingExperience}</p>
-                    <p className='text-xl text-black font-semibold block'>Working Hospital: {DoctorInfo.doctorWorkingHospital}</p>
-                    <p className='text-xl text-black font-semibold block'>Working Hospital Address: {DoctorInfo.doctorWorkingHospitalAddress}</p>
-                    <p className='text-xl text-black font-semibold block'>Working hours start at: {DoctorInfo.doctorWorkingHoursStart}</p>
-                    <p className='text-xl text-black font-semibold block'>Working hours end at: {DoctorInfo.doctorWorkingHoursEnd}</p>
+                    <p className='text-xl text-black font-semibold block'>Name: {doctor.name}</p>
+                    <p className='text-xl text-black font-semibold block'>Email: {doctor.email}</p>
+                    <p className='text-xl text-black font-semibold block'>Phone: {doctor.phone_numer}</p>
+                    <p className='text-xl text-black font-semibold block'>Working Experience: {doctor.working_experience}</p>
+                    <p className='text-xl text-black font-semibold block'>Working Hospital: {doctor.hospital_id.facility_name}</p>
+                    <p className='text-xl text-black font-semibold block'>Working Hospital Address: {doctor.hospital_id.address}</p>
+                    <p className='text-xl text-black font-semibold block'>Working hours start at: {doctor.working_hours.start_time}</p>
+                    <p className='text-xl text-black font-semibold block'>Working hours end at: {doctor.working_hours.end_time}</p>
                 </div>
             </div>
             <div className=' mt-20 flex justify-center'>
