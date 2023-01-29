93c7ca77a0f0d5bd4bb8b0cb14969cf7c3b37adc
chong-zhanhang <chongzhanhang@hotmail.com>
2023-01-29 13:21:48 +0800

    Fixed a bug in Appointment page

--- icare/src/pages/Appointment.js
+++ icare/src/pages/Appointment.js
@@ -6,7 +6,7 @@ export default function Appointment() {
             <div className="ml-20">
                 <h1 className='text-5xl text-primary font-bold pt-16 pl-10 mb-20'>Appointments</h1>
                 <div>
-                    <form className = "pl-10" onSubmit={ handleSave }>
+                    <form className = "pl-10">
                         <label>
                             <span className="text-2xl text-black font-semibold block">Location:</span>
                             <input className='bg-white border-2 rounded-lg border-gray-400' type="text" required />
